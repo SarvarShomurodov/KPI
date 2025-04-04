aa5a4be0c66a6971e074cff22c0b8e11ca397908
Shomurodov Sarvar <s.shomurodov@1.imrs>
2025-04-04 17:54:24 +0500

    changes

--- resources/views/admin/roles/create.blade.php
+++ resources/views/admin/roles/create.blade.php
@@ -1,32 +1,39 @@
 @extends('layouts.admin')
 
 @section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Rollarni qo'shish</h2>
+        </div>
+        <div class="pull-right">
+          @can('create-user')
+            <a class="btn btn-primary mb-2" href="{{ route('roles.index') }}">&larr; Orqaga</a>
+          @endcan
+        </div>
+    </div>
+  </div>
+  @if (session('success'))
+      <div class="alert alert-success">
+          {{ session('success') }}
+      </div>
+  @endif
     <div class="col-md-4">
-
         <div class="card">
-            <div class="card-header">
-                <div class="float-start">
-                    Add New Role
-                </div>
-                <div class="float-end">
-                    <a href="{{ route('roles.index') }}" class="btn btn-primary btn-sm">&larr; Back</a>
-                </div>
-            </div>
             <div class="card-body">
                 <form action="{{ route('roles.store') }}" method="post">
                     @csrf
 
                     <div class="mb-3">
-                        <label for="name" class="col-md-4 col-form-label text-md-end text-start">Name</label>
+                        <label for="name" class="form-label">Name</label>
                           <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') }}">
                             @if ($errors->has('name'))
                                 <span class="text-danger">{{ $errors->first('name') }}</span>
                             @endif
                     </div>
 
-                    <div class="mb-3 row">
-                        <label for="permissions" class="col-md-4 col-form-label text-md-end text-start">Permissions</label>
-                        <div class="col-md-6">           
+                    <div class="mb-3">
+                        <label for="permissions" class="form-label">Permissions</label>        
                             <select class="form-select @error('permissions') is-invalid @enderror" multiple aria-label="Permissions" id="permissions" name="permissions[]" style="height: 210px;">
                                 @forelse ($permissions as $permission)
                                     <option value="{{ $permission->id }}" {{ in_array($permission->id, old('permissions') ?? []) ? 'selected' : '' }}>
@@ -39,11 +46,10 @@
                             @if ($errors->has('permissions'))
                                 <span class="text-danger">{{ $errors->first('permissions') }}</span>
                             @endif
-                        </div>
                     </div>
                     
-                    <div class="mb-3 row">
-                        <input type="submit" class="col-md-3 offset-md-5 btn btn-primary" value="Add Role">
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-primary me-2" value="Add Role">
                     </div>
                     
                 </form>
